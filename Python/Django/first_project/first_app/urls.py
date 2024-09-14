0dd8c7b342ff6c867cfcbf24c39041506bb29cf0
abshir24 <abshirm@live.com>
2024-09-14 09:20:43 -0700

    added code djA3

--- Python/Django/first_project/first_app/urls.py
+++ Python/Django/first_project/first_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path(r'', views.index),
     path('displayinfo', views.displayinfo),
     path('displaytime', views.displaytime),
-    path('displayname/<name>', views.displayname)
+    path('display-name/<name>', views.displayname),
+    path('display-food/<food>', views.displayfood),
+    path('display-vacation/<vacation>', views.displayvacation)
 ]
--- Python/Django/first_project/first_app/views.py
+++ Python/Django/first_project/first_app/views.py
@@ -3,10 +3,11 @@ from datetime import datetime
 # Create your views here.
 
 def index(request):
-    name = "Abshir"
-    return render(request, "index.html", context = {"name":name})
+    return render(request, "index.html")
 
+# --------------------------------------------------------
 def displayinfo(request):
+    # CODE FOR ASSIGNMENT 1
     name = "Abshir"
     food = "Indian food"
     movie = "Black Panther 1!"
@@ -14,11 +15,26 @@ def displayinfo(request):
 
     return HttpResponse(f"Name: {name} Favorite Food: {food} Movie: {movie} Music Genre: {music}")
 
+# --------------------------------------------------------
+
+# --------------------------------------------------------
+
 def displaytime(request):
+
+    # CODE FOR ASSIGNMENT 2
     current_time = datetime.now()
 
     return render(request, "time.html",context = {"current_time": current_time} )
 
 
+# --------------------------------------------------------
+
+# CODE FOR ASSIGNMENT 3
 def displayname(request, name):
-    return render(request, "name.html", context = {"name": name} )
+   return render(request, "name.html" , context = { "name": name } ) 
+
+def displayfood(request, food):
+   return render(request, "food.html" , context = { "food": food } ) 
+
+def displayvacation(request, vacation):
+   return render(request, "vacation.html" , context = { "vacation": vacation } ) 
