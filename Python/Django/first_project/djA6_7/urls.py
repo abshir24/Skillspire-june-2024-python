e7e47360ef412bf53803498d46cf20f5df4fe3cb
abshir24 <abshirm@live.com>
2024-09-19 18:51:51 -0700

    added coded for dj assignment 6

---
+++ Python/Django/first_project/djA6_7/urls.py
@@ -0,0 +1,10 @@
+# Inside first_project/first_app/urls.py
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(r'', views.index),
+    path('addpost', views.addpost)
+]
--- Python/Django/first_project/first_project/urls.py
+++ Python/Django/first_project/first_project/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path(r'', include('djA6_7.urls')),
     # path(r'', include('djA4.urls')),
-    path(r'', include('demo.urls')),
+    # path(r'', include('demo.urls')),
     # path(r'', include('first_app.urls')),
+
     path("admin/", admin.site.urls),
 ]
